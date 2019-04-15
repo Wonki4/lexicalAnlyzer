75db406db4af0ac5c1dba81e30fa9ead60423b7d
Wonkiki <qordnjsrl13@naver.com>
2019-04-15 22:42:19 +0900

    JM

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("hello, world")
-print("hello, world")
-print("hello, world")
+"""
+The classes of token are keyword, identifier, comparison, float and whitespace etc..
+"""
+
+letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+digit = ['0','1','2','3','4','5','6','7','8','9','0']
+skip = [" ","\r","\n","\n"]
+
