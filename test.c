ada27042ae2a50cafbe9fba582ea5f209a70b056
Wonki4 <qordnjsrl13@naver.com>
2020-09-28 21:39:09 +0900

    Implementing DFA

---
+++ test.c
@@ -0,0 +1,19 @@
+int func(int a) {
+
+   char string = "every body";
+
+   int num1 = -999;
+
+   int num2 = 999;
+
+   if(num1!=num2){
+
+      num1=num2
+   }else{
+      reuturn 0;
+   }
+
+   int num3.num4;
+
+   return 0;
+}
